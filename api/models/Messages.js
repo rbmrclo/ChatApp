4f520cb1ecc76537464520eb78e7ad0910a0f8bc
Robbie Marcelo <rbmrclo@hotmail.com>
2013-08-08 20:44:30 +0800

    Initial commit with love <3

---
+++ api/models/Messages.js
@@ -0,0 +1,7 @@
+module.exports = {
+    attributes  : {
+        userId: 'INT',
+        username: 'STRING',
+        message: 'STRING'     
+    }
+};
---
+++ api/models/Users.js
@@ -0,0 +1,6 @@
+module.exports = {
+    attributes  : {
+         username: 'STRING',
+         password: 'STRING'     
+    }
+};
